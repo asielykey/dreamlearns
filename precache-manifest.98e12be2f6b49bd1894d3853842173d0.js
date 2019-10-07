2efcf4eb0785077adf4c92d445cb1ff5ac14da0c
asielykey <54544856+asielykey@users.noreply.github.com>
2019-10-07 16:20:30 +0700

    Updates

---
+++ precache-manifest.98e12be2f6b49bd1894d3853842173d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1688104953a4afe8c90e01711df041",
+    "url": "/newapp/index.html"
+  },
+  {
+    "revision": "42b41dea1a48e1ad0304",
+    "url": "/newapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5191d930005ea493c3f4",
+    "url": "/newapp/static/js/2.6451a434.chunk.js"
+  },
+  {
+    "revision": "42b41dea1a48e1ad0304",
+    "url": "/newapp/static/js/main.6cecc919.chunk.js"
+  },
+  {
+    "revision": "f1a9cdea90a9f0f3e8f0",
+    "url": "/newapp/static/js/runtime-main.aca2128d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/newapp/static/media/logo.25bf045c.svg"
+  }
+]);
